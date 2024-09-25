4d24a3b0437324e28f66cff139848a3f73f03aee
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-09-25 19:06:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ def is_enabled(value, default):
     else:
         return default
 
-AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002237286662').split()] 
+AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002169369654').split()] 
 
 
 UPDATE = environ.get("SUPPORT", "")
@@ -21,49 +21,49 @@ OWNERID = int(environ.get("OWNERID" ,"6359874284"))
 YT = environ.get("YT", "")
 
 # Bot Information
-API_ID = int(environ.get("API_ID", "25492855"))
-API_HASH = environ.get("API_HASH", "61876db014de51a4ace6b169608be4f1")
-BOT_TOKEN = environ.get("BOT_TOKEN", "nUbAA-nNBQ")
+API_ID = int(environ.get("API_ID", "25928407"))
+API_HASH = environ.get("API_HASH", "d0824f557b06f6c87b0b0ce1e7c2f1e4")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7541145414:AAHfT6ce6IPrcScfI9aJCtS6VKK3nA2PY_E")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/34384a407532bf5738721.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6359874284').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "FileShare_Hgbot") # without @
+PICS = (environ.get('PICS', '')).split() # Bot Start Picture
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7299439165').split()]
+BOT_USERNAME = environ.get("BOT_USERNAME", "Tanushreetokenbot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "HGBOTZ")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodbue&wCluster0")
-DB_NAME = environ.get("DB_NAME", "HGBOTZ")
+DB_URI = environ.get("DB_URI", "mongodb+srv://lord4gent02:4eucQeoUuYLSGTD7@cluster0.r0asv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "HgfileStore")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "180")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "10800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002155023199"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002488839738"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 
 # Enable - True or Disable - False
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
 
 # Verify Info :-
-VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
+VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "publicearn.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "54335b798ddf24239b147c499daeb1f629951839") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/ultroid_official/18") # how to open link 
 
 # Website Info:
